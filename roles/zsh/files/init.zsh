88d878cad1bb3539f25398275a494a5af54a960d
Ryan S. Brown <sb@ryansb.com>
2015-07-10 08:36:44 -0400

    Fix paths in zsh configs

--- roles/zsh/files/init.zsh
+++ roles/zsh/files/init.zsh
@@ -1,6 +1,6 @@
 zstyle -a ':prezto:module:custom' hackmode 'hackmodes'
 if (( $#hackmodes > 0 )); then
-  source ~/.dotfiles/zsh/custom/zaw/zaw.zsh
+  source ~/.zsh/custom/zaw/zaw.zsh
   bindkey '^R' zaw-history
   bindkey -M filterselect '^R' down-line-or-history
   bindkey -M filterselect '^E' up-line-or-history
