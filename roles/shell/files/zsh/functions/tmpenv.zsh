c36e5ee77df7e26ca2c422a105aac98b3e9fa01a
Ryan S. Brown <sb@ryansb.com>
2018-05-25 15:19:25 -0400

    Update to python 2.7.15

--- roles/shell/files/zsh/functions/tmpenv.zsh
+++ roles/shell/files/zsh/functions/tmpenv.zsh
@@ -1,6 +1,6 @@
 function tmpenv {
     venv_name="tmpenv-$(cat /dev/urandom | tr -dc '0-9a-zA-Z' | head -c15)"
-    pyenv virtualenv "${1:-2.7.12}" ${venv_name}
+    pyenv virtualenv "${1:-2.7.15}" ${venv_name}
     pyenv activate $venv_name
 }
 
