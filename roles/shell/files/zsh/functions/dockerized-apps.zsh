4e399594510bb5eec5575e8db3b52985051f08f5
Ryan S. Brown <sb@ryansb.com>
2018-06-01 16:08:04 -0400

    Add dockerized slack setup

---
+++ roles/shell/files/zsh/functions/dockerized-apps.zsh
@@ -0,0 +1,43 @@
+# copied from https://github.com/jessfraz/dotfiles/blob/master/.dockerfunc
+function del_stopped {
+	local state
+	state=$(docker inspect --format "{{.State.Running}}" "${1}" 2>/dev/null)
+
+	if [[ "$state" == "false" ]]; then
+		docker rm "${1}"
+	fi
+}
+
+function slack-focused {
+    del_stopped focus-slack
+    docker run -d \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e "DISPLAY=unix${DISPLAY}" \
+        --device /dev/snd \
+        --device /dev/dri \
+        --device /dev/video0 \
+        --group-add audio \
+        --group-add video \
+        -v "${HOME}/.slack/focused-slack:/root/.config/Slack" \
+        --ipc="host" \
+        --name focus-slack \
+        ryansb/slack
+}
+
+function slack-all {
+    del_stopped all-slack
+    docker run -d \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e "DISPLAY=unix${DISPLAY}" \
+        --device /dev/snd \
+        --device /dev/dri \
+        --device /dev/video0 \
+        --group-add audio \
+        --group-add video \
+        -v "${HOME}/.slack/all-slack:/root/.config/Slack" \
+        --ipc="host" \
+        --name all-slack \
+        ryansb/slack
+}
