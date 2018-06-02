d30cf351188b3e5405c3d9b3058c5609f154b6a0
Ryan S. Brown <sb@ryansb.com>
2018-06-02 11:30:53 -0400

    Add Zoom

--- roles/shell/files/zsh/functions/dockerized-apps.zsh
+++ roles/shell/files/zsh/functions/dockerized-apps.zsh
@@ -80,3 +80,19 @@ function bluejeans {
         --name bluejeans \
         ryansb/bluejeans
 }
+
+function zoom {
+    del_stopped zoom
+    docker run -d \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e "DISPLAY=unix${DISPLAY}" \
+        --device /dev/snd \
+        --device /dev/dri \
+        --device /dev/video0 \
+        --group-add audio \
+        --group-add video \
+        --ipc="host" \
+        --name zoom \
+        ryansb/zoom-us
+}
